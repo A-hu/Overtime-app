fd97e68409f3fcd8ec6c097b204782987a207072
A-hu <louis0911317399@gmail.com>
2017-02-06 16:41:45 +0800

    Used the resource generator for posts

--- db/schema.rb
+++ db/schema.rb
@@ -7,12 +7,20 @@
 # system, you should be using db:schema:load, not running all the migrations
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
+#
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170206075506) do
+ActiveRecord::Schema.define(version: 20170206083054) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.date     "date"
+    t.text     "rationale"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
