d63c3f0e757557eb35e82a92b400fede573efd86
A-hu <louis0911317399@gmail.com>
2017-02-06 19:15:49 +0800

    Implemented user connection with tests

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 describe 'navigate' do
   describe 'index' do
+    before do
+      user = User.create(email: "test_post@test.com", password: "sdfsddsfsd", first_name: "hhihi", last_name: "hello")
+      login_as(user, scope: :user)
+    end
     it 'can be reached successfully' do
       visit posts_path
       expect(page.status_code).to eq(200)
@@ -15,6 +19,8 @@ describe 'navigate' do
 
   describe 'creation' do
     before do
+      user = User.create(email: "test_post@test.com", password: "sdfsddsfsd", first_name: "hhihi", last_name: "hello")
+      login_as(user, scope: :user)
       visit new_post_path
     end
     it 'has a form that can be reached' do
@@ -24,10 +30,16 @@ describe 'navigate' do
     it 'can be created from new form page' do
       fill_in 'post[date]', with: Date.today
       fill_in 'post[rationale]', with: "Some rationale"
-
       click_on "Save"
 
       expect(page).to have_content("Some rationale")
+    end
+
+    it 'will have a user associated it' do
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: "User_Association"
+      click_on "Save"
+      expect(User.last.posts.last.rationale).to eq("User_Association")
     end
   end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
   describe "Creation" do
     before do
-     @post = Post.create(date: Date.today, rationale: "hello")
+     user = User.create(email: "test_post@test.com", password: "sdfsddsfsd", first_name: "hhihi", last_name: "hello")
+     @post = Post.create(date: Date.today, rationale: "hello", user_id: user.id)
     end
    it 'can be created' do
      expect(@post).to be_valid
