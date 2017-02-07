0d5f187200c078ac5003e0094fc2b577e29e72fa
A-hu <louis0911317399@gmail.com>
2017-02-07 15:56:16 +0800

    Updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,8 @@
+@user = User.create(email: "hihi@hi.com", password: "123123", first_name: "hihi", last_name: "yes")
+
+puts "1 user created"
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content")
+  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
 end
 
 puts "100 Posts have been created"
