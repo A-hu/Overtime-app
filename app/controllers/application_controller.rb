1833ca08dc1ed321cd884172c5cae6c45f23471f
A-hu <louis0911317399@gmail.com>
2017-02-13 18:08:32 +0800

    Setup skeleton employee homepage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,14 +5,15 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def admin_types
+    ['AdminUser']
+  end
   private
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit( :sign_up, keys: [:first_name, :last_name] )
     devise_parameter_sanitizer.permit( :account_update, keys: [:first_name, :last_name] )
   end
-
-  private
 
   def user_not_authorized
     flash[:alert] = "You are not authorized to perform this action."
