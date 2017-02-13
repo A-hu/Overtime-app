eebceb3bb0a399df79b00150f476da6151cb853a
A-hu <louis0911317399@gmail.com>
2017-02-13 12:30:32 +0800

    Integrated application helper for status labels and integrated enums for audit log

--- app/models/audit_log.rb
+++ app/models/audit_log.rb
@@ -1,5 +1,8 @@
 class AuditLog < ApplicationRecord
+  enum status: { pending: 0, confirmed: 1 }
+
   belongs_to :user
+
   validates :user_id, :status, :start_date, presence: true
 
   after_initialize :set_defaults
