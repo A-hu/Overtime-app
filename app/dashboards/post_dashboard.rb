a1d02dd24834821c5cde094f4aa66e9f0869f5cf
A-hu <louis0911317399@gmail.com>
2017-02-17 10:44:29 +0800

    Added Overtime request to post dashboard

--- app/dashboards/post_dashboard.rb
+++ app/dashboards/post_dashboard.rb
@@ -12,6 +12,7 @@ class PostDashboard < Administrate::BaseDashboard
     id: Field::Number.with_options(searchable: false),
     date: Field::DateTime.with_options(searchable: false),
     rationale: Field::Text.with_options(searchable: true),
+    overtime_request: Field::Number.with_options(searchable: true),
     status: Field::String.with_options(searchable: true),
     created_at: Field::DateTime.with_options(searchable: false),
     updated_at: Field::DateTime.with_options(searchable: false),
@@ -27,6 +28,7 @@ class PostDashboard < Administrate::BaseDashboard
     :status,
     :date,
     :rationale,
+    :overtime_request,
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
@@ -36,6 +38,7 @@ class PostDashboard < Administrate::BaseDashboard
     :status,
     :date,
     :rationale,
+    :overtime_request,
     :created_at,
     :updated_at,
   ].freeze
@@ -47,6 +50,7 @@ class PostDashboard < Administrate::BaseDashboard
     :user,
     :date,
     :rationale,
+    :overtime_request,
   ].freeze
 
   # Overwrite this method to customize how posts are displayed
